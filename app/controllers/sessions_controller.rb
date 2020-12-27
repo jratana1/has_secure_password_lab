6f0af0f76971ce73707e85da5ab70dc1354a151b
John Ratana <jratana1@gmail.com>
2020-12-26 20:49:42 -0500

    commit tree 2379deb332699a36186f1b1d2512685046790eb0

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,23 +4,11 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(name: params[:user][:name])
-        user = user.try(:authenticate, params[:user][:password])
-    
-        return redirect_to login_path unless user
-    
-        session[:user_id] = user.id
-    
-    
-        redirect_to home_path
+        if user && user.authenticate(params[:user][:password])
+           session[:user_id] = user.id
+           redirect_to home_path
+        else
+           redirect_to login_path
+        end
     end
-
-    # def create
-    #     user = User.find_by(name: params[:name])
-    #     if user && user.authenticate(params[:password])
-    #        session[:user_id] = user.id
-    #        redirect_to home_path
-    #     else
-    #        redirect_to login_path
-    #     end
-    # end
 end
